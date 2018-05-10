9598f9bfc8839f9046f8a6a13e58503f46de1a5a
zhoujiaping <421211679@qq.com>
2018-05-10 22:21:46 +0800

    add comment

--- nodenode/algol/mcs.js
+++ nodenode/algol/mcs.js
@@ -2,6 +2,16 @@ const {cache,time,compose} = require('./aop');
 /**
  * 最长公共子序列的问题
  * http://www.cnblogs.com/huangxincheng/archive/2012/11/11/2764625.html
+ * 求序列X={x0,x1,x2,....xi}
+ * 与序列Y={y0,y1,y2,...yj}
+ * 的最长公共子序列。
+ * 公式：
+ * f(i,j) = '' if(i==0 || j==0)
+ * f(i,j) = f(i-1,j-1)+1 if(i>0 && j>0 && xi==yj)
+ * f(i,j) = max(f(i-1,j),f(i,j-1)) if(i>0 && j>0 && xi!=yj)
+ * 代码实现是比较简单的，最重要的是证明该公式。
+ * 第一种情况，X序列的长度为0，或者Y序列的长度为0，那么最长公共子序列为''，这个太简单不需要证明。
+ * 第二种情况，用反正法，假设f(i,j)>f(i-1,j-1)+1，即f(i-1,j-1)<f(i,j)-1。
  * 
  */
 function mcs(seqA,seqB){
